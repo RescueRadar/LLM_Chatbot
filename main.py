5ac30df3132cfd700b3cd575b3a76ecbcd7b6b5c
abhisek1221 <cherrymx317@gmail.com>
2023-09-24 09:26:50 +0530

    document loader

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ from langchain.document_loaders import DirectoryLoader
 
 from langchain.chat_models import ChatOpenAI
 llm = ChatOpenAI(temperature=0,model_name="gpt-3.5-turbo")
+
+import os
+os.environ["OPENAI_API_KEY"]= os.getenv("API_KEY")
+
+pdf_loader = DirectoryLoader(os.getenv("DIR_PATH"), glob="**/*.pdf")
+txt_loader = DirectoryLoader(os.getenv("DIR_PATH"), glob="**/*.txt")
+word_loader = DirectoryLoader(os.getenv("DIR_PATH"), glob="**/*.docx")
+
+loaders = [pdf_loader, txt_loader , word_loader]
+documents = []
+for loader in loaders:
+    documents.extend(loader.load())
