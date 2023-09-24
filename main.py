7d2fac99eaecf70657480c9fce611f750ec61b5a
abhisek1221 <cherrymx317@gmail.com>
2023-09-24 09:32:50 +0530

    Embeddings, Interface , Vectorization of DB

--- main.py
+++ main.py
@@ -18,3 +18,34 @@ loaders = [pdf_loader, txt_loader , word_loader]
 documents = []
 for loader in loaders:
     documents.extend(loader.load())
+
+text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+documents = text_splitter.split_documents(documents)
+
+embeddings = OpenAIEmbeddings()
+vectorstore = Chroma.from_documents(documents, embeddings)
+
+qa = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever(), return_source_documents=True)
+
+import gradio as gr
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.Button("Clear")
+    chat_history = []
+    def user(query, chat_history):
+        print("User query:", query)
+        print("Chat history:", chat_history)
+
+        chat_history_tuples = []
+        for message in chat_history:
+            chat_history_tuples.append((message[0], message[1]))
+        result = qa({"question": query, "chat_history": chat_history_tuples})
+        chat_history.append((query, result["answer"]))
+        print("Updated chat history:", chat_history)
+        return gr.update(value=""), chat_history
+    msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False)
+    clear.click(lambda: None, None, chatbot, queue=False)
+
+if __name__ == "__main__":
+    demo.launch(debug=True,share=True
