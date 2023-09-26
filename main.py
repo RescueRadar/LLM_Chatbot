3b2151130ac3484a6b76947042932f58a6af4881
abhisek1221 <cherrymx317@gmail.com>
2023-09-26 12:32:23 +0530

    env secured + inframe tab addded to streamlit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+import streamlit.components.v1 as components
+import os
+from dotenv import load_dotenv
+load_dotenv()
 def perform_action(option):
     if option == 'Agency Query':
         st.write('Here is your query chatbot')
@@ -7,15 +10,17 @@ def perform_action(option):
         from langchain.vectorstores import Chroma
         from langchain.text_splitter import CharacterTextSplitter
         from langchain.chains import ConversationalRetrievalChain
-        import os
-        os.environ["OPENAI_API_KEY"]= os.getenv("API_KEY")
+        
+
+        os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
+
+        from langchain.chat_models import ChatOpenAI
         llm = ChatOpenAI(temperature=0,model_name="gpt-3.5-turbo")
 
-        txt_loader = DirectoryLoader(os.getenv("DIR_PATH"), glob="**/*.txt")
-        word_loader = DirectoryLoader(os.getenv("DIR_PATH"), glob="**/*.docx")
-        from langchain.chat_models import ChatOpenAI
-
         from langchain.document_loaders import DirectoryLoader
+
+        txt_loader = DirectoryLoader(os.getenv('DIR_PATH'), glob="**/*.txt")
+        word_loader = DirectoryLoader(os.getenv('DIR_PATH'), glob="**/*.docx")
 
         loaders = [txt_loader , word_loader]
         documents = []
@@ -55,14 +60,16 @@ def perform_action(option):
 
             msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False)
             clear.click(lambda: None, None, chatbot, queue=False)
-
+        components.iframe("http://127.0.0.1:7860", width=800,height=600,scrolling=True)
         if __name__ == "__main__":
             demo.launch(debug=True,share=True)
+
     elif option == 'General Query':
         st.write('Here is your query chatbot')
+
         import openai 
         import gradio  
-        openai.api_key=os.getenv("API_KEY")
+        openai.api_key = os.getenv('OPENAI_API_KEY')
         messages=[{"role":"system","content":"you are a rescue agency"}]  
 
         def CustomChatGPT(user_input):
@@ -76,7 +83,7 @@ def perform_action(option):
             return ChatGPT_reply
 
         demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Calamity chatbot")
-
+        components.iframe("http://127.0.0.1:7862", width=800, height=600, scrolling=True)
         demo.launch(share=True)
 
 st.title('Select Chatbot type')
@@ -85,3 +92,4 @@ option_selected = st.radio('Select an option:', ('Agency Query', 'General Query'
 
 if st.button('Perform Action'):
     perform_action(option_selected)
+    
